eff7d6870c1911f2e22213055ef603fd7242b803
Rodrigo Yoshimoto <rodrigo.yoshimoto@gmail.com>
2019-03-21 22:00:10 -0300

    added change password routes and functionality, added to heroku the mail validation

--- routes/auth.js
+++ routes/auth.js
@@ -89,7 +89,7 @@ router.post("/sign-up", (req, res, next) => {
               from: "ih-feedback.herokuapp.com",
               to: email,
               subject: "Welcome to Feedback!",
-              html: `In order to use our app, please click <a href="http://localhost:3000/confirm/${emailConfirmationCode}">here</a> to confirm your e-mail.
+              html: `In order to use our app, please click <a href="${process.env.APP_URI}/confirm/${emailConfirmationCode}">here</a> to confirm your e-mail.
               `
             })
             .then(info => console.log("nodemailer success -->", info))
@@ -120,7 +120,7 @@ router.get("/confirm/resend/:emailConfirmationCode", (req, res) => {
           from: "ih-feedback.herokuapp.com",
           to: user.email,
           subject: "[RESEND] Welcome to Feedback!",
-          html: `In order to use our app, please click <a href="http://localhost:3000/confirm/${emailConfirmationCode}">here</a> to confirm your e-mail.
+          html: `In order to use our app, please click <a href="${process.env.APP_URI}/confirm/${emailConfirmationCode}">here</a> to confirm your e-mail.
         `
         })
         .then(info => console.log("nodemailer success -->", info))
@@ -185,7 +185,7 @@ router.post("/recover", (req, res, next) => {
             from: "ih-feedback.herokuapp.com",
             to: user.email,
             subject: "[FORGOT PASSWORD] Confirm your e-mail",
-            html: `Please click <a href="http://localhost:3000/recover/${resetPasswordCode}">here</a>
+            html: `Please click <a href="${process.env.APP_URI}/recover/${resetPasswordCode}">here</a>
           `
           })
           .then(info => console.log("nodemailer success -->", info))
@@ -260,5 +260,119 @@ router.post("/recover/change-password", (req, res, next) => {
       throw new Error(err);
     });
 });
+
+router.get("/edit-profile", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  User.findById(req.user._id)
+    .then(user => {
+      res.render("auth/user-details", { user, error: req.flash("error"), success: req.flash("success") });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.post("/edit-profile", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+
+  const { email, fullName } = req.body;
+
+  if (email == "" || fullName == "") {
+    if(fullName.length == 1){
+      req.flash("error", "Your name should have at least two characters")
+      res.redirect("/edit-profile")
+      return;  
+    }
+    req.flash("error", "Please fill all fields.")
+    res.redirect("/edit-profile")
+    return;
+  }
+
+  const updateUser = { email, fullName };
+
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: updateUser },
+    { new: true }
+  )
+    .then(user => {
+      req.flash("success", "Your profile has been updated.");
+      res.redirect("/edit-profile");
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+});
+
+router.get("/edit-profile/change-password", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  res.render("auth/password-change", { error: req.flash("error") });
+});
+
+router.post("/edit-profile/change-password", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const {email, currentPassword, password, passwordCheck} = req.body;
+
+  if (password == "" || currentPassword == "" || passwordCheck == "" ) {
+    res.render("auth/password-change", {
+      error: `Please fill all fields.`
+    });
+    return;
+  }
+ 
+  if (password == currentPassword) {
+    res.render("auth/password-change", {
+      error: `Please choose a password different from your current password.`
+    });
+    return;
+  }
+
+
+  if (password !== passwordCheck) {
+    res.render("auth/password-change", {
+      error: `New password doesn't match.`
+    });
+    return;
+  }
+
+  if (!bcrypt.compareSync(currentPassword, req.user.password)){
+    res.render("auth/password-change", {
+      error: `Current password doesnt match.`
+    });
+    return;
+  }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let emailConfirmationCode = "";
+  for (let i = 0; i < 25; i++) {
+    emailConfirmationCode +=
+      characters[Math.floor(Math.random() * characters.length)];
+  }
+
+    User.findOneAndUpdate({email},{ $set: {status: 'Pending', emailConfirmationCode: emailConfirmationCode, password: hashPass }},
+    {new: true })
+  .then(user => {
+    if ((user)){ 
+      transporter
+        .sendMail({
+          from: "ih-feedback.herokuapp.com",
+          to: user.email,
+          subject: "[PASSWORD CHANGE] E-mail confirmation required",
+          // text: message,
+          html: `Please click <a href="${process.env.APP_URI}/confirm/${confirmationCode}">here</a>
+          `
+        })
+        .then(info => console.log("nodemailer success -->", info))
+        .catch(error => console.log(error));
+
+    req.logout();
+    req.flash('success', "Your password have been changed, please confirm your email.")
+    res.redirect("/login");
+      } 
+  })
+  .catch(err => { throw new Error(err)})
+
+});
+
 
 module.exports = router;
